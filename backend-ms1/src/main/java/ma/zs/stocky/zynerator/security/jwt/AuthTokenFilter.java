985c1596a733f6d31969cedaa20332ba2af5a030
HibaEssamih <essamih.hiba@gmail.com>
2024-02-01 16:04:39 +0100

    sakai-v1

--- backend-ms1/src/main/java/ma/zs/stocky/zynerator/security/jwt/AuthTokenFilter.java
+++ backend-ms1/src/main/java/ma/zs/stocky/zynerator/security/jwt/AuthTokenFilter.java
@@ -41,7 +41,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-            response.addHeader("Access-Control-Allow-Origin", "*");// Angular :: "http://localhost:4200" or React :: "http://localhost:3000"
+            response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");// Angular :: "http://localhost:4200" or React :: "http://localhost:3000"
             response.addHeader("Access-Control-Allow-Headers",
                     "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
             response.addHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, PUT, PATCH, OPTIONS");
